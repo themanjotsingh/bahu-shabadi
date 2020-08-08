5dbb8d696ac3d300bcdd25d1dd4fcce6638cadbd
Manjot Singh <github@manjotsingh.xyz>
2020-08-08 01:51:04 -0400

    Update shakas.js

--- resources/shakas.js
+++ resources/shakas.js
@@ -1,48 +1,13 @@
-numbers = ["one",
-"two",
-"three",
-"four",
-"five",
-"six",
-"seven",
-"eight",
-"nine",
-"ten",
-"eleven",
-"twelve",
-"thirteen",
-"fourteen",
-"fifteen",
-"sixteen",
-"seventeen",
-"eighteen",
-"nineteen",
-"twenty",
-"twentyone",
-"twentytwo",
-"twentythree",
-"twentyfour"]
-
-
 apiline = "https://api.gurbaninow.com/v2/line/"
 apishabad = "https://api.gurbaninow.com/v2/shabad/"
-
-
-
-
-var pathArray = window.location.pathname.split('/');
-shakanum = pathArray[1].replace(/\D/g, "");
-writtenshakanum = numbers[shakanum - 1]
 
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('shaka');
 shakanum = myParam
 
-
 $.getJSON(
     "https://sheetdb.io/api/v1/aqv99npuj23ip?sheet=sheet",
     function (data) {
-
 
         lineslist = data[shakanum - 1].lines
         lines = lineslist.split(",")
@@ -69,7 +34,6 @@ $.getJSON(
         aftershabadslist = data[shakanum - 1].shabads
         aftershabads = aftershabadslist.split(",")
 
-
         for (var i = 0; i < aftershabads.length; i++) {
             var each = aftershabads[i];
 
@@ -82,7 +46,6 @@ $.getJSON(
                     for (var i = 0; i < data.shabad.length; i++) {
                         var each = data.shabad[i];
 
-
                         var div = document.getElementById("bani2");
 
                         var shabad = document.createElement('p');
@@ -90,25 +53,10 @@ $.getJSON(
                         shabad.className = "shabad"
                         div.appendChild(shabad);
 
-
                     }
 
                 });
         };
 
-
-
-
-
-
-
-    });
-
-
-
-
-$.getJSON(
-    "https://api.gurbaninow.com/v2/shabad/MK1",
-    function (data) {
 
     });
