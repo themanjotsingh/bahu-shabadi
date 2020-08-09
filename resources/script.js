909bed1a022483e4af952c4bfd04d692f977f5b4
Manjot Singh <github@manjotsingh.xyz>
2020-08-08 23:14:01 -0400

    Add files via upload

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,6 @@
+const checkq = new URLSearchParams(window.location.search);
+const whatsq = checkq.get('shaka');
+
 $.getJSON(
     "https://adv.sikhresources.site/json/shakas.json",
     function (data) {
@@ -8,7 +11,9 @@ $.getJSON(
             var shakasdiv = document.getElementById("myleftnav");
             
             var link = document.createElement('a');
-
+            if (whatsq == shaka.shaka) {
+                link.className = "active"
+            }
             link.setAttribute("href", shaka.link);
             link.appendChild(document.createTextNode("ਸ਼ਕਾ " + shaka.shaka));
             shakasdiv.appendChild(link);
@@ -38,5 +43,33 @@ function rightopenNav() {
 function rightcloseNav() {
     document.getElementById("myrightnav").style.width = "0";
 }
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 
+if (getCookie("shabad") == "") {
+    document.cookie = "shabad=ps; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+
+}
+
+if (getCookie("english") == "") {
+    document.cookie = "english=no; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+
+}
+if (getCookie("punjabi") == "") {
+    document.cookie = "punjabi=no; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+
+}
--- resources/shakas.js
+++ resources/shakas.js
@@ -5,13 +5,31 @@ const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('shaka');
 shakanum = myParam
 
+if (shakanum == null) {
+    window.location.replace("/");
+}
+
+if (shakanum < 1) {
+    window.location.replace("/");
+}
+
+if (shakanum > 24) {
+    window.location.replace("/");
+}
+
+
+
 $.getJSON(
-    "https://sheetdb.io/api/v1/aqv99npuj23ip?sheet=sheet",
+    "shabads.json",
     function (data) {
 
-        lineslist = data[shakanum - 1].lines
-        lines = lineslist.split(",")
-
+        try {
+            lineslist = data[shakanum - 1].lines
+            lines = lineslist.split(",")
+        } catch (err) {
+            console.log("nolines")
+            lines = []
+        }
         lines.forEach(myLine)
 
         function myLine(item, index) {
@@ -27,8 +45,48 @@ $.getJSON(
 
                     var shabad = document.createElement('p');
                     shabad.appendChild(document.createTextNode(data.line.gurmukhi.unicode));
-                    shabad.className = "shabad"
+                    if (getCookie("shabad") == "lv") {
+                        shabad.className = "shabad hidden"
+                    } else if (getCookie("shabad") == "ps") {
+                        shabad.className = "shabad"
+                    }
+
                     div.appendChild(shabad);
+
+
+                    var larivaar = document.createElement('p');
+                    larivaar.appendChild(document.createTextNode(data.line.larivaar.unicode));
+                    if (getCookie("shabad") == "ps") {
+                        larivaar.className = "larivaar hidden"
+                    } else if (getCookie("shabad") == "lv") {
+                        larivaar.className = "larivaar"
+                    }
+                    div.appendChild(larivaar);
+
+
+                    var punjabi = document.createElement('p');
+                    punjabi.appendChild(document.createTextNode(data.line.translation.punjabi.default.unicode));
+                    if (getCookie("punjabi") == "no") {
+                        punjabi.className = "punjabi hidden"
+                    } else if (getCookie("punjabi") == "yes") {
+                        punjabi.className = "punjabi"
+                    }
+                    div.appendChild(punjabi);
+
+
+                    var english = document.createElement('p');
+                    english.appendChild(document.createTextNode(data.line.translation.english.default));
+                    if (getCookie("english") == "no") {
+                        english.className = "english hidden"
+                    } else if (getCookie("english") == "yes") {
+                        english.className = "english"
+                    }
+                    div.appendChild(english);
+
+
+
+
+
                 });
         };
 
@@ -56,14 +114,127 @@ $.getJSON(
 
                         var shabad = document.createElement('p');
                         shabad.appendChild(document.createTextNode(each.line.gurmukhi.unicode));
-                        shabad.className = "shabad"
+                        if (getCookie("shabad") == "lv") {
+                            shabad.className = "shabad hidden"
+                        } else if (getCookie("shabad") == "ps") {
+                            shabad.className = "shabad"
+                        }
                         div.appendChild(shabad);
 
+
+                        var larivaar = document.createElement('p');
+                        larivaar.appendChild(document.createTextNode(each.line.larivaar.unicode));
+                        if (getCookie("shabad") == "ps") {
+                            larivaar.className = "larivaar hidden"
+                        } else if (getCookie("shabad") == "lv") {
+                            larivaar.className = "larivaar"
+                        }
+                        div.appendChild(larivaar);
+
+
+                        var punjabi = document.createElement('p');
+                        punjabi.appendChild(document.createTextNode(each.line.translation.punjabi.default.unicode));
+                        if (getCookie("punjabi") == "no") {
+                            punjabi.className = "punjabi hidden"
+                        } else if (getCookie("punjabi") == "yes") {
+                            punjabi.className = "punjabi"
+                        }
+                        div.appendChild(punjabi);
+
+
+                        var english = document.createElement('p');
+                        english.appendChild(document.createTextNode(each.line.translation.english.default));
+                        if (getCookie("english") == "no") {
+                            english.className = "english hidden"
+                        } else if (getCookie("english") == "yes") {
+                            english.className = "english"
+                        }
+                        div.appendChild(english);
+
+
                     }
-
-
 
                 })
         }
 
-    })
+    });
+
+
+backlink = "/shaka.html?shaka=" + (parseInt(shakanum) - 1)
+document.getElementById("back").setAttribute("href", backlink);
+nextlink = "/shaka.html?shaka=" + (parseInt(shakanum) + 1)
+document.getElementById("next").setAttribute("href", nextlink);
+
+
+function togglelarivaar() {
+
+
+    if (getCookie("shabad") == "lv") {
+        $(".shabad").addClass("hidden");
+        document.cookie = "shabad=ps; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".larivaar").removeClass("hidden");
+        document.getElementById("shabad").innerHTML = "Pad Shed";
+
+
+    } else if (getCookie("shabad") == "ps") {
+        $(".larivaar").addClass("hidden");
+
+        document.cookie = "shabad=lv; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".shabad").removeClass("hidden");
+        document.getElementById("shabad").innerHTML = "Larivaar";
+
+    }
+
+
+
+}
+
+function toggleenglish() {
+
+
+    if (getCookie("english") == "yes") {
+        document.cookie = "english=no; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".english").addClass("hidden");
+
+    } else if (getCookie("english") == "no") {
+        document.cookie = "english=yes; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".english").removeClass("hidden");
+    }
+
+
+
+}
+
+function togglepunjabi() {
+
+
+    if (getCookie("punjabi") == "yes") {
+        document.cookie = "punjabi=no; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".punjabi").addClass("hidden");
+
+    } else if (getCookie("punjabi") == "no") {
+        document.cookie = "punjabi=yes; expires=Thu, 18 Dec 2023 12:00:00 UTC";
+        $(".punjabi").removeClass("hidden");
+    }
+
+
+
+}
+
+
+
+
+
+
+if (getCookie("shabad") == "lv") {
+    document.getElementById("shabad").innerHTML = "Pad Shed";
+
+} else if (getCookie("shabad") == "ps") {
+
+    document.getElementById("shabad").innerHTML = "Larivaar";
+
+
+
+}
+
+console.log(getCookie("shabad"))
